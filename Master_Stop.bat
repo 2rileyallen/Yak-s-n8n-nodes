b0dd2bd422a010ba9e05641e0e78887b7eccdb27
2rileyallen <2rileyallen@gmail.com>
2025-08-24 09:14:52 -0600

    master stop change

--- Master_Stop.bat
+++ Master_Stop.bat
@@ -10,5 +10,5 @@ ECHO --- Shutting down n8n Server... ---
 powershell -command "Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like '*n8n*' -and $_.Name -eq 'node.exe' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }"
 
 ECHO.
-ECHO --- All services have been shut down. ---
-PAUSE
+ECHO --- All services have been shut down. Window will close in 5 seconds... ---
+timeout /t 5 >nul
